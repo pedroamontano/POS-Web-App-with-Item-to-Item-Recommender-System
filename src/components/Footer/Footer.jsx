87f024f2a3b57b5e8cd04c20b2269228e2ceba76
Jeff Ma <jeff.ma713@gmail.com>
2023-08-25 10:36:51 -0700

    footer completed without active state

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,29 @@
-import cartIcon from "../../../assets/icons/outline/shopping bag.svg";
-import searchIcon from "../../../assets/icons/outline/search.svg";
-import scanIcon from "../../../assets/icons/outline/barcode.svg";
-import storeIcon from "../../../assets/icons/outline/store.svg";
+import cartIcon from "../../assets/icons/outline/shopping bag.svg";
+import searchIcon from "../../assets/icons/outline/search.svg";
+import scanIcon from "../../assets/icons/outline/barcode.svg";
+import storeIcon from "../../assets/icons/outline/store.svg";
+import "./Footer.scss";
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer__cart-and-search">
-        <div className="footer__cart">
-          <img src={cartIcon} alt="cart logo" className="footer__image" />
-          <p className="footer__text"> CART </p>
-        </div>
-
-        <div className="footer__search">
-          <img src={searchIcon} alt="search logo" />
-          <p className="footer__text"> SEARCH </p>
-        </div>
+      <div className="footer__sub-section">
+        <img src={cartIcon} alt="cart logo" className="footer__image" />
+        <p className="footer__text"> CART </p>
       </div>
-      <div className="footer__scan-and-store">
-        <div className="footer__scan">
-          <img src={scanIcon} alt="scan logo" />
-          <p className="footer__text"> SCAN </p>
-        </div>
-        <div className="footer__store">
-          <img src={storeIcon} alt="store logo" />
-          <p className="footer__text"> STORE </p>
-        </div>
+
+      <div className="footer__sub-section">
+        <img src={searchIcon} alt="search logo" className="footer__image" />
+        <p className="footer__text"> SEARCH </p>
+      </div>
+
+      <div className="footer__sub-section">
+        <img src={scanIcon} alt="scan logo" className="footer__image" />
+        <p className="footer__text"> SCAN </p>
+      </div>
+
+      <div className="footer__sub-section">
+        <img src={storeIcon} alt="store logo" className="footer__image" />
+        <p className="footer__text"> STORE </p>
       </div>
     </div>
   );
